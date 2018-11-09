d9357ed5f98ec39bc8f7c52fec58749c96758ad5
Daniel Andres Charry <danielcharry@Woloxs-MacBook-Pro-2.local>
2018-11-09 15:26:23 -0500

    subir proyecto

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,9 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Daniel Andres Charry on 09/11/2018.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+import Foundation
---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Daniel Andres Charry on 09/11/2018.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
